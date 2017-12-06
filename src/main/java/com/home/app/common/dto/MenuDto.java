0a3323735216cdac25428accb9f6c85da323a926
hyuns <hyuns@JunHyunSooPc>
2017-12-06 23:39:54 +0900

    현재까지 작업내용 커밋

---
+++ src/main/java/com/home/app/common/dto/MenuDto.java
@@ -0,0 +1,52 @@
+package com.home.app.common.dto;
+
+public class MenuDto {
+	private int no;
+
+	private String menuId;
+	private String menuName;
+
+	private String regDate;
+	private String useYn;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getUseYn() {
+		return useYn;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+
+}
